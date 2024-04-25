7c30b1d920dfa1b76e26f204b6a87cadbe3d17de
Yamini Chandana <yaminichandana3@gmail.com>
2024-04-25 09:18:40 +0530

    Changed view of restaurant

--- app/RestaurantCard.tsx
+++ app/RestaurantCard.tsx
@@ -4,8 +4,8 @@ export default function RestaurantCard(props: { restaurant: {} }) {
   const restaurant = props.restaurant.info;
   return (
     <Link href = {`/restaurants/${restaurant.id}`}>
-    <div className='w-48 bg-gray-200 p-4'>
-        <img className='h-48 w-full' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant.cloudinaryImageId}`}></img>
+    <div className='w-56 bg-gray-200 p-4'>
+        <img className='w-full h-44 rounded-lg' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant.cloudinaryImageId}`}></img>
         <b><h2>{restaurant.name}</h2></b>
         <h4>{restaurant.cuisines.join(", ")}</h4>
       <h3>{restaurant.avgRating}</h3>
